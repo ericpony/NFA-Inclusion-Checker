5b3534aaa309b1e6ac95f721e5b3ee97f81410ab
ericpony <ericpony@csie.org>
2013-06-24 00:14:29 +0800

    main

--- main.php
+++ main.php
@@ -1,3 +1,3 @@
 <?
-passthru('echo '.escapeshellarg($_POST['aag']).' > test.aag ; aiger-1.9.4/aigtoaig test.aag test.aig ; abc/abc -f abc.sh');
+passthru('echo '.escapeshellarg($_POST['aag']).' > test.aag ; aiger-1.9.4/aigtoaig test.aag test.aig ; abc/abc -c "read test.aig; pdr"');
 ?>
